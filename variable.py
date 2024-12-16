b67e1e07f2ce4cf5b44ddecb59a25a93f80448a9
srinivas <gantasrinu019@gmail.com>
2024-12-16 11:33:48 +0530

    var

--- variable.py
+++ variable.py
@@ -3,5 +3,52 @@
 # b=8
 # print(a,b)
 
-a,b=5,6
+a=2
+b=5
 print(a,b)
+
+# Variables declare a variable with Alpanumeric characters. 
+# a to z or A to Z or Number to number
+# a=10 or A=12 or cnu=9
+
+# we cannot declear variable starting with number.
+# Ex : 1a=44 
+
+# if we want to declear a variable with number then we have to give ( _ )
+# Ex :_1a=24 using with Underscore
+
+# if we want to declear a variable two (or) more words then give ( _ ) underscore between those words 
+# Ex :Hie_tech=44
+
+# Variable Names is 32 Character
+
+# IDE --> means- Integrated Development Environment
+# application using --> visual studio
+
+
+# types of Variables
+# there are two types
+# 1) Global variable 
+# 2) locals variable
+# t=77
+# def sample ():
+#     t=24
+#     print(t)
+# function calling <--simple ()
+# print(t)
+
+# global variable: The variable which we declear outside of fuction is called global variable.
+# local variable: The variable which we declear inside of fuction is called local variable.
+
+
+# different types of prints 
+# name="cnu"
+# age=23
+# print(name,age)
+# # or
+# print("My name is",name,"My age is",age)
+# # Or
+# print("My name is %s and My age is %d"%(name,age))
+# # or
+# print("My name is ",name,"and My age is ",age)
+
