5cb08fdcbdf60cfcc66b12f8a56939ae31aae7e5
srinivas <gantasrinu019@gmail.com>
2024-12-16 13:07:41 +0530

    con

---
+++ Control Statements/Conditional Statement.py
@@ -0,0 +1,25 @@
+#what is the control statements ?
+#one statement will control set of instruction is called control statements.
+
+# Types of control Statements
+# there are 3 types 
+# 1)Condiational statement
+# 2)looping Statement 
+# 3)Transfer statement (or) jumping statement
+
+# 1)Conditional statement: if,if else,elif,nestedif.
+# if : if the conditional is true if frints the if Block
+ 
+# Example
+# a=10
+# b=20
+# if b>a:
+#  print("b is greater than a")
+
+#if else example
+# age =24
+# experience =4
+# if age >23 and experience >3:
+#     print("Eligible.")
+# else:
+#     print("Not Eligible.")
