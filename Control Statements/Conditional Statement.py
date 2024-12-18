64bb4024a96c7efe0d4946217c5c5ffcc60a302c
srinivas <gantasrinu019@gmail.com>
2024-12-18 21:32:12 +0530

    hsk

--- Control Statements/Conditional Statement.py
+++ Control Statements/Conditional Statement.py
@@ -23,3 +23,52 @@
 #     print("Eligible.")
 # else:
 #     print("Not Eligible.")
+
+#write a program even and odd?
+# number=int(input("Enter the number:"))
+# if number%2==0:
+#     print("its even number")
+# else:
+#     print("its odd number")
+
+# write a program to print the school grade marks?
+# marks=int(input("Enter the marks:"))
+# if marks>=80 and marks<100:
+#     print("A grade")
+# elif marks>=60 and marks<80:
+#     print("b grade")
+# elif marks>=40 and marks<60:
+#     print("c grade")
+# else: 
+#     print("sorry your fail")
+
+# # write a programe calculate?
+# a=int(input("Enter the a value:"))
+# b=int(input("Enter the b value:"))
+# choice=input("Enter the choice:")
+# print ("press'+' to add '-' to sub '*' to mul:")
+# if choice=='+':
+#     print(a+b)
+# elif choice=='-':
+#     print(a-b)
+# elif choice=='*':
+#     print(a*b)
+# else:
+# #     print("please select the operator")
+
+# #  write a programe a company decide to give bouns to newyear to their empoly?
+# 1.m=>5%?
+# 2.f=>10%?
+# 3.salary<=15000 =>3%?
+# 4.find out salary?
+# salary=int(input("Enter the salary:"))
+# Gender=input("Enter the Gender:")
+# if Gender=="m":
+#     bonus=0.05*salary
+# else:
+#     bonus=0.10*salary
+# if salary <=15000:
+#     bonus=bonus+0.03*salary
+# salary=bonus+salary
+# print("salary with bonus is:",salary)
+
