36e7620bb67a2fde6147141856081b60d05c462e
srinivas <gantasrinu019@gmail.com>
2024-12-20 18:45:02 +0530

    5hf

---
+++ looping statements.py/while loop.py
@@ -0,0 +1,36 @@
+# what is looping statement?
+# if you we want to any task agian and again then we can proceed  with looping concerpt like while and for and nested.
+
+# # 3 types of looping?
+# whlie and for and nested
+
+#  while loop?
+# if we know the desination the we can proceed with while loop.
+
+# write a programe to print 1 to 10?
+# start,end,step
+# # 1,10,1
+# i=1
+# while i<=10:
+#     print(i)
+#     i=i+1
+
+# write a programe to print even number 1 to 10 number?
+# i=2
+# while i<=10:
+#     print(i)
+#     i=i+2 
+
+# write a programe to print run time program?
+# i=int(input("Enter the start value:"))
+# n=int(input("enter the stop value:"))
+# while i<=n:
+#     print(i)
+#     i=i+1    
+
+# write a programe to print table number?
+# table_number=int(input("Enter the table number:"))
+# i=1
+# while i<=10:
+#     print(f"{table_number}*{i}={table_number}")
+#     i=i+1
