3addff1bdb5e8288ead135d915e343921a7a37bf
srinivas <gantasrinu019@gmail.com>
2024-12-30 16:30:06 +0530

    hfds

---
+++ patters.py/patters.py
@@ -0,0 +1,149 @@
+# <!-- write a programe to print stars? -->
+# n=5
+# for i in range(n):
+#     print("*",end=" ")
+    
+'''
+* * * * * 
+'''
+
+# wirte a programe to print square?
+# n=5
+# for i in range(n):
+#     for j in range(n):
+#         print("*",end=" ")
+#     print()
+    
+# output
+# * * * * * 
+# * * * * * 
+# * * * * *
+# * * * * *
+# * * * * *
+
+# #  wirte a programe to print haif pyramid?
+# n=5
+# for i in range(n):
+#     for j in range(i+1):
+#         print("*",end=" ")
+#     print()
+    
+# output
+# *
+# * *
+# * * *
+# * * * *
+# * * * * *
+
+# wirte a programe to print inverted haif pyramid?
+# n=5
+# for i in range(n):
+#     for j in range(i,n):
+#         print("*",end=" ")
+#     print( )
+    
+# output
+# * * * * * 
+# * * * * 
+# * * * 
+# * * 
+# * 
+
+# #  wirte a programe to print mirror haif pyramid?
+# n=5
+# for i in range(n):
+#     for j in range(i+1):
+#         print(" ",end=" ")
+#     for j in range(i,n):
+#         print("*",end=" ")
+#     print()
+    
+# output
+# * * * * * 
+#     * * * * 
+#       * * * 
+#         * * 
+#           * 
+
+#  wirte a programe to print inverted haif pyramid?
+# n=5
+# for i in range(n):
+#     for j in range(i,n):
+#         print(" ",end=" ")
+#     for j in range(i+1):
+#         print("*",end=" ")
+#     print()
+
+# output
+#           * 
+#         * * 
+#       * * *
+#     * * * *
+#   * * * * *
+
+# wirte a programe to print full pyramid?
+# n=5
+# for i in range(n):
+#     for j in range(i,n):
+#         print(" ",end=" ")
+#     for j in range(i+1):
+#         print("*",end=" ")
+#     for j in range(i):
+#         print("*",end=" ")
+#     print()
+    
+# output
+#           * 
+#         * * * 
+#       * * * * *
+#     * * * * * * *
+#   * * * * * * * * *
+
+# wirte a programe to print full pyramid?
+# n=5
+# for i in range(n):
+#     for j in range(i+1):
+#         print(" ",end=" ")
+#     for j in range(i,n):
+#         print("*",end=" ")
+#     for j in range(i,n-1):
+#         print("*",end=" ")
+#     print()
+    
+# output
+#  * * * * * * * * * 
+#     * * * * * * * 
+#       * * * * *
+#         * * *
+#           *
+
+# wirte a programe to print dimond stars?
+# n=5
+# for i in range(n-1):
+#     for j in range(i,n):
+#         print(" ",end=" ")
+#     for j in range(i+1):
+#         print("*",end=" ")
+#     for j in range(i):
+#         print("*",end=" ")
+#     print()
+    
+# n=5
+# for i in range(n):
+#     for j in range(i+1):
+#         print(" ",end=" ")
+#     for j in range(i,n):
+#         print("*",end=" ")
+#     for j in range(i,n-1):
+#         print("*",end=" ")
+#     print()
+    
+# output
+#           * 
+#         * * * 
+#       * * * * *
+#     * * * * * * *
+#   * * * * * * * * *
+#     * * * * * * *
+#       * * * * *
+#         * * *
