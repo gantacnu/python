e0957f9e3722efa23545576ad082decff97ad3f2
srinivas <gantasrinu019@gmail.com>
2024-12-30 15:32:30 +0530

    vj

---
+++ sample.py
@@ -0,0 +1,43 @@
+# <!-- write a programe to print stars? -->
+# n=5
+# for i in range(n):
+#     print("*",end=" ")
+    
+'''
+* * * * * 
+'''
+
+# wirte a programe to print square?
+# n=5
+# for i in range(n):
+#     for j in range(n):
+#         print("*",end=" ")
+#     print()
+    
+# output
+# * * * * * 
+# * * * * * 
+# * * * * *
+# * * * * *
+# * * * * *
+
+# #  wirte a programe to print haif pyramid?
+# n=5
+# for i in range(n):
+#     for j in range(i+1):
+#         print("*",end=" ")
+#     print()
+    
+# output
+# *
+# * *
+# * * *
+# * * * *
+# * * * * *
+
+# wirte a programe to print inverted haif pyramid?
+n=5
+for i in range(n):
+    for j in range(i,n):
+        print("*",end=" ")
+    print( )
